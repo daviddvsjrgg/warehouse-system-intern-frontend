11658aece4890c1e9c40e29fc8e19d3d90a327ca
David <daviddwiyanto79@gmail.com>
2024-10-24 13:37:42 +0700

    fix: margin-x report date filter

--- src/components/report/TableReport.tsx
+++ src/components/report/TableReport.tsx
@@ -54,7 +54,7 @@ const TableReport: React.FC = () => {
   return (
     <div className="overflow-x-auto">
       {/* Filter Inputs */}
-      <div className='flex justify-between mt-2 mr-2'>
+      <div className='flex justify-between mt-2 mr-2 mx-2'>
         <div className="gap-4 mb-4 mt-2">
           <div className='flex'>
             <input
@@ -74,13 +74,40 @@ const TableReport: React.FC = () => {
             />
           </div>
         </div>
-          <input
-            type="text"
-            value={skuSearch}
-            onChange={(e) => setSkuSearch(e.target.value)}
-            className="input input-bordered"
-            placeholder="Search by SKU"
-          />
+          <div className="max-w-md mb-3">
+            <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
+              Search
+            </label>
+            {error && <div className="text-red-500">{error}</div>}
+            <div className="relative">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg
+                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </div>
+              <input
+                type="text"
+                value={skuSearch}
+                onChange={(e) => setSkuSearch(e.target.value)}
+                id="default-search"
+                className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"
+                placeholder="Cari sku, nama barang"
+                required
+              />
+            </div>
+          </div>
       </div>
       <table className="table">
         {/* Table Head */}
