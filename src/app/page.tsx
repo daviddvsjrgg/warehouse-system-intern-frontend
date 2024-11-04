0e70f9f65e6158e04c398ea5a0d1c05178205c1c
David <daviddwiyanto79@gmail.com>
2024-11-04 11:41:30 +0700

    feat: dashboard description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,11 @@ const Home = () => {
         {/* Main content */}
         <main className="p-4 flex-1">
           <h1 className="text-2xl font-bold">Selamat Datang di Warehouse System</h1>
-          <p className="mt-2">Terakhir Deploy Senin, 3 November 2024</p>
+          <p className="mt-2">Terakhir Deploy Senin, 4 November 2024</p>
+          <br />
+          <p>Frontend: <a className='text-blue-500 underline' href="https://anker.highking.cloud"target='_blank' rel="noopener noreferrer">anker.highking.cloud</a></p>
+          <p>Backend: <a className='text-blue-500 underline' href="https://api-backend-ws-david-intern.vercel.app"target='_blank' rel="noopener noreferrer">api-backend-ws-david-intern.vercel.app</a></p>
+          <p>API Doc: <a className='text-blue-500 underline' href="https://documenter.getpostman.com/view/10569515/2sAY4xA2Ec"target='_blank' rel="noopener noreferrer">dokumentasi</a></p>
           {/* Additional content can go here */}
         </main>
       </div>
