70a03b3f2c12bf38c08ac5aed2ee7935e3f15507
daviddvsjrgg <daviddwiyanto79@gmail.com>
2024-11-23 15:43:16 +0700

    fix: build nextjs to export

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,10 @@ import type { Config } from "tailwindcss";
 import daisyui from "daisyui"
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+   content: [
+    './pages/**/*.{js,ts,jsx,tsx}',  // Scan all files in the pages directory
+    './components/**/*.{js,ts,jsx,tsx}',  // Scan all files in the components directory
+    './out/**/*.html',  // Include static HTML files after export
   ],
   theme: {
     extend: {
