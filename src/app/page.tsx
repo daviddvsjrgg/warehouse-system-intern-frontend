0c912ea722e95da3e1191ad1f7faffc94065404b
daviddvsjrgg <daviddwiyanto79@gmail.com>
2025-01-10 11:39:30 +0700

    docs: update push date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ const Home = () => {
                 Selamat Datang di Warehouse System
               </h1>
               <p className="mt-2 dark:text-gray-300">
-                Terakhir Push: <span className="font-medium">Senin, 6 Januari 2024, 13:04 WIB</span>
+                Terakhir Push: <span className="font-medium">Jum`at, 10 Januari 2024, 11:40 WIB</span>
               </p>
 
               {/* Links Section */}
