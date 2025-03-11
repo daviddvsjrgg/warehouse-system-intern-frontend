c16a707f1c26688f00272560ab3e02ae886bbd7f
daviddvsjrgg <daviddwiyanto79@gmail.com>
2025-03-12 04:15:17 +0700

    fix: build next js into production

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'img.daisyui.com',
-          },
-        ],
-      },
+  images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'img.daisyui.com',
+        },
+      ],
+  },
+  output: 'export',
 };
 
 export default nextConfig;
