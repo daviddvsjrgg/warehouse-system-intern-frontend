d38e29549276231ae70531d484a5822696052c2e
daviddvsjrgg <daviddwiyanto79@gmail.com>
2025-03-12 08:57:27 +0700

    fix: handle npm run build both index.html and .next build

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'img.daisyui.com',
-        },
-      ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.daisyui.com',
+      },
+    ],
   },
+  output: 'export', // Full static export
+  trailingSlash: true, // Needed for static routing
 };
 
 export default nextConfig;
